cea2b489f50dadddc52566f02cf155f68b691add
Donatas <petrdonatas@gmail.com>
2023-01-29 11:14:34 +0200

    Refactor

--- string/toCamelCase.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(value) {
-    return value.replace(/[^a-zA-Z0-9]+(.)/g, function (_, chr) { return chr.toUpperCase(); });
-}
-exports.default = default_1;
