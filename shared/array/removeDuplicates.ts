ad60f42d09b09b13a5e476e54213317b355b0d83
Donatas <petrdonatas@gmail.com>
2023-09-19 21:54:51 +0300

    Update

--- shared/array/removeDuplicates.ts
+++ shared/array/removeDuplicates.ts
@@ -1,3 +1,11 @@
 export default function (inputArray: string[]) {
-	return Array.from(new Set(inputArray));
+	return Array.from(new Set(inputArray))
+		.filter((item) => {
+			// todo this filter is temporary, remove after fixing project build
+			// when this was created the problem was that there are scripts in stricts
+			// that reads all endpoints(and similar) from folders and they would make duplicates
+			// such as myEndpoint.ts and myEndpoint.js which further rabbithole creates
+			// duplication errors
+			return !item.endsWith('.js');
+		});
 }
