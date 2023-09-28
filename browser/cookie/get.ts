b699a9a146213e85e10105704f3f540d9b4b0c0a
Donatas <petrdonatas@gmail.com>
2023-09-28 21:59:54 +0300

    Update

--- browser/cookie/get.ts
+++ browser/cookie/get.ts
@@ -1,13 +1,15 @@
 export default function(name: string): string | undefined {
-	const cookieStr = document.cookie;
-	const cookies = cookieStr.split(';');
+	if (typeof document !== 'undefined') {
+		const cookieStr = document.cookie;
+		const cookies = cookieStr.split(';');
 
-	for (const cookie of cookies) {
-		const [cookieName, cookieValue] = cookie.split('=');
-		const trimmedCookieName = cookieName.trim();
+		for (const cookie of cookies) {
+			const [cookieName, cookieValue] = cookie.split('=');
+			const trimmedCookieName = cookieName.trim();
 
-		if (trimmedCookieName === name) {
-			return decodeURIComponent(cookieValue);
+			if (trimmedCookieName === name) {
+				return decodeURIComponent(cookieValue);
+			}
 		}
 	}
 
